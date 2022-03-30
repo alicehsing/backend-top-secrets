3a173563d8cc81dfdcf10c1a978d8bae7d892ad3
Alice Hsing <alicehsing@gmail.com>
2022-03-29 18:13:24 -0700

    wrote test for log in an exisiting user, added controller, service and model; passed test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const UserService = require('../lib/services/UserService');
 
 describe('backend-top-secrets routes', () => {
   beforeEach(() => {
@@ -14,12 +15,28 @@ describe('backend-top-secrets routes', () => {
 
   it('registers a user via POST', async () => {
     const res = await request(app)
-      .post('/api/v1/users/sessions')
+      .post('/api/v1/users')
       .send({ email: 'momothecow@momo.com', password: 'iliketoeatgrass' });
 
     expect(res.body).toEqual({ 
       id: expect.any(String), 
       email: 'momothecow@momo.com' });
+  });
+
+  it('logs in an existing user', async () => {
+    const user = await UserService.create({
+      email: 'momothecow@momo.com',
+      password: 'iliketoeatgrass',
+    });
+
+    const res = await request(app)
+      .post('/api/v1/users/sessions')
+      .send({ email: 'momothecow@momo.com', password: 'iliketoeatgrass' });
+
+    expect(res.body).toEqual({ 
+      message: 'You have signed in successfully!',
+      user,
+    });
   });
 
 });
